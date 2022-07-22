12e09b94adeacc10e2aefd41f8024d02c1fbf6ce
mpetereit <100138349+mpetereit@users.noreply.github.com>
2022-07-22 11:10:17 +0200

    FPIT Commands file
    Powershell File that generates the output to pre-fill fields in WebJEA

---
+++ Build/Template/StarterFiles/FPIT_Commands.ps1
@@ -0,0 +1,47 @@
+param(
+    [string]$EXEC_Code,
+	[string]$EXEC_Value
+    )
+	
+	import-module ActiveDirectory
+
+#Umlaute Encoding
+[Console]::OutputEncoding = [System.Text.UTF8Encoding]::new($false)
+
+#FPITLBXXX -> Creates Listbox
+#FPITSLXXX -> Creates Testbox
+
+
+
+
+if ($EXEC_Code -eq "FPITSL002")
+{
+	if ($EXEC_Value -eq "rueckgabe1")
+	{
+		return "zu_webjea_1";
+	}
+	elseif ($EXEC_Value -eq "rueckgabe3" )
+	{
+		return "zu_webjea_2";
+	}
+	else
+	{
+		return "zu_webjea_3";
+	}
+}
+elseif($EXEC_Code -eq "FPITLB020")
+{
+	$blub = @()
+	$blub = (get-adgroup -Filter * -Properties * | select Name).Name
+	$Ausgabe = ""
+	foreach($user in $blub)
+	{
+		$Ausgabe = $Ausgabe + $user + ";"
+	}
+
+	return $Ausgabe
+}
+else
+{
+	return "zurueck"
+}
